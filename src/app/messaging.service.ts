7cfe4378579e54945e29f5395a8fc070f469e77c
delgado.trueba <delgado.trueba@outlook.es>
2019-08-16 17:56:51 +0200

    New Push Notification Service

--- src/app/messaging.service.ts
+++ src/app/messaging.service.ts
@@ -6,7 +6,7 @@ import 'firebase/messaging';
 import { environment } from '../environments/environment';
 
 import { from, throwError, Observable, BehaviorSubject } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,9 @@ export class MessagingService {
   private messaging;
   private _state: BehaviorSubject<string>;
 
+  // getToken return null
+  private token: string = null;
+
   public get state() {
     return this._state.asObservable();
   }
@@ -24,9 +27,35 @@ export class MessagingService {
     try {
       const app = firebase.initializeApp(environment.firebaseConfig);
       this.messaging = app.messaging();
-      this._state = new BehaviorSubject( Notification.permission );
+      this._state = new BehaviorSubject(Notification.permission);
+
+      this.messaging.onTokenRefresh(() => {
+        this.getToken().subscribe( tokenRefresh => {
+          if (this.token !== null){
+            //eliminar token
+          }
+          //añadir token;
+          this.token = tokenRefresh;
+        });
+      });
+
     } catch (err) {
-      this._state = new BehaviorSubject( 'not-supported' );
+      this._state = new BehaviorSubject('not-supported');
+    }
+  }
+
+  public async enabledNotification() {
+    let permission = await this.askForPermissionToReceiveNotifications();
+
+    if (this.isGranted(permission)) {
+      this.getToken().subscribe( (tokenRefresh) => {
+        if (this.token !== null){
+          //eliminar token
+        }
+        //añadir token;
+        this.token = tokenRefresh;
+        this.listenNotification();
+      });
     }
   }
 
@@ -46,17 +75,6 @@ export class MessagingService {
     return permission === 'not-supported';
   }
 
-  public async enabledNotification() {
-    let permission = await this.askForPermissionToReceiveNotifications();
-
-    if (this.isGranted(permission)) {
-      this.getToken().subscribe( (token) => {
-        console.log(token);
-        this.listenNotification();
-      });
-    }
-  }
-
   private askForPermissionToReceiveNotifications() {
     return Notification.requestPermission()
       .then( permission => {
@@ -67,19 +85,15 @@ export class MessagingService {
   }
 
   private getToken(): Observable<string> {
-    // sendTokenToServer(currentToken);
-    // updateUIForPushEnabled(currentToken);
-
-    // Show permission UI.
-    // updateUIForPushPermissionRequired();
-    // setTokenSentToServer(false);
 
     const tokenPromise = this.messaging.getToken()
       .then((currentToken) => {
         return new Promise((resolve, reject) => {
           if (currentToken) {
+            console.log('TOKEN: ' + currentToken);
             resolve(currentToken);
           } else {
+            console.log('TOKEN: ' + currentToken);
             reject(
               new Error('No Instance ID token available. Request permission to generate one.')
             );
@@ -90,7 +104,6 @@ export class MessagingService {
     return from<string>(tokenPromise)
       .pipe(
         catchError( err => {
-          console.log('An error occurred while retrieving token. ');
           return throwError(err);
         })
       );
